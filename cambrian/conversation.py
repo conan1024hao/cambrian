a9a80ae6ae43b9d9513f6794773a76a8275f8482
王昊 <conan1024hao@gmail.com>
2025-01-02 01:03:28 -0500

    .

--- cambrian/conversation.py
+++ cambrian/conversation.py
@@ -627,8 +627,8 @@ conv_phi3 = Conversation(
 # default_conversation = conv_chatml_direct
 # default_conversation = conv_vicuna_v1
 
-# default_conversation = conv_gemma_instruct
-default_conversation = conv_llama_3
+# default_conversation = conv_llama_3
+default_conversation = conv_gemma_instruct
 
 conv_templates = {
     "default": conv_vicuna_v0,
