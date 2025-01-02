6ce951117f5c1840dbfe3f1850006727e34067ce
王昊 <conan1024hao@gmail.com>
2025-01-01 23:28:19 -0500

    .

--- cambrian/train/train_fsdp.py
+++ cambrian/train/train_fsdp.py
@@ -2066,9 +2066,9 @@ def train(INDEX, attn_implementation=None):
     elif model_args.version == "llama_v3":
         tokenizer.pad_token = "<|reserved_special_token_0|>"
         tokenizer.pad_token_id = 128002
-    # elif model_args.version == "gemma":  # HACK
-    #     tokenizer.pad_token = "<unused0>"
-    #     tokenizer.pad_token_id = 7
+    elif model_args.version == "gemma":  # HACK
+        tokenizer.pad_token = "<unused0>"
+        tokenizer.pad_token_id = 7
     else:
         tokenizer.pad_token = tokenizer.unk_token
         if model_args.version in conversation_lib.conv_templates:
